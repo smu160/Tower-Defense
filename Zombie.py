37f62b4b4c8559e8e25fdc79c2758de7e2b8e7a3
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-03-12 02:45:19 -0400

    Created move() function that takes care of moving zombies across the screen

--- Zombie.py
+++ Zombie.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sun Mar 11 23:57:17 2018
@@ -7,16 +6,24 @@ Created on Sun Mar 11 23:57:17 2018
 """
 
 import pygame
+import random
 
 
 class Zombie:
     "Common base class for all zombies"
     zombie_count = 0
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, game_display):
         self.x = x
         self.y = y
+        self.game_display = game_display
+        self.health = 100
+        self.image = pygame.image.load("zombie.png")
         Zombie.zombie_count += 1
+
+    def move(self):
+        self.x += random.uniform(0.1, 0.9)
+        self.game_display.blit(self.image, (self.x, self.y))
 
     def displayCount(self):
         print("Total zombies {}".format(Zombie.zombie_count))
