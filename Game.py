fd0ece7cac8dc4f507e801c5c3bf19c1eb9e6e53
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-03-12 02:44:49 -0400

    Moved zombie functionality to Zombie class and added more zombies

--- Game.py
+++ Game.py
@@ -6,6 +6,7 @@ Created on Sun Mar 11 20:37:14 2018
 @author:
 """
 import pygame
+from Zombie import *
 
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 768
@@ -13,7 +14,6 @@ WINDOW_HEIGHT = 768
 LIGHT_GREY = (240, 240, 240)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
 
 zombies_past_perimeter = 0
 
@@ -32,10 +32,6 @@ def tower(x, y):
     game_display.blit(tower_img, (x, y))
 
 
-def zombie(x, y):
-    game_display.blit(zombie_img, (x, y))
-
-
 x_tower = WINDOW_WIDTH * 0.85
 y_tower = WINDOW_HEIGHT * 0.3
 
@@ -43,6 +39,18 @@ x_zombie = WINDOW_WIDTH * 0.01
 y_zombie = WINDOW_HEIGHT * 0.5
 
 crashed = False
+
+zombie_0 = Zombie(x_zombie, WINDOW_HEIGHT * 0.0, game_display)
+zombie_1 = Zombie(x_zombie, WINDOW_HEIGHT * 0.1, game_display)
+zombie_2 = Zombie(x_zombie, WINDOW_HEIGHT * 0.2, game_display)
+zombie_3 = Zombie(x_zombie, WINDOW_HEIGHT * 0.3, game_display)
+zombie_4 = Zombie(x_zombie, WINDOW_HEIGHT * 0.4, game_display)
+zombie_5 = Zombie(x_zombie, WINDOW_HEIGHT * 0.5, game_display)
+zombie_6 = Zombie(x_zombie, WINDOW_HEIGHT * 0.6, game_display)
+zombie_7 = Zombie(x_zombie, WINDOW_HEIGHT * 0.7, game_display)
+zombie_8 = Zombie(x_zombie, WINDOW_HEIGHT * 0.8, game_display)
+zombie_9 = Zombie(x_zombie, WINDOW_HEIGHT * 0.9, game_display)
+zombie_10 = Zombie(x_zombie, WINDOW_HEIGHT * 1.0, game_display)
 
 while not crashed:
     for event in pygame.event.get():
@@ -52,9 +60,19 @@ while not crashed:
     game_display.fill(LIGHT_GREY)
     tower(x_tower, y_tower)
 
-    x_zombie += 5
-    if x_zombie > 0 and x_zombie < x_tower:
-        zombie(x_zombie, y_zombie)
+    zombie_0.move()
+    zombie_1.move()
+    zombie_2.move()
+    zombie_3.move()
+    zombie_4.move()
+    zombie_5.move()
+    zombie_6.move()
+    zombie_7.move()
+    zombie_8.move()
+    zombie_9.move()
+    zombie_10.move()
+    # if x_zombie > 0 and x_zombie < x_tower:
+    #    zombie(x_zombie, y_zombie)
 
     pygame.display.update()
     clock.tick(60)
