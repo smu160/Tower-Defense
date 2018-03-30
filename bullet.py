ea09fbcd47719c779005671f2aa5abeb9aabb61f
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-03-29 22:57:29 -0400

    First commit

---
+++ bullet.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 29 22:43:27 2018
+
+@authors: Saveliy Yusufov and Michael Harley
+"""
+import pygame
+
+BLACK = (0, 0, 0)
+
+
+class Bullet(pygame.sprite.Sprite):
+    "Common base class for all bullets"
+
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([4, 10])
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        """Overriden sprite function
+
+        Args:
+        """
+        self.rect.x -= 3
---
+++ cannon.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 29 22:43:28 2018
+
+@authors: Saveliy Yusufov and Michael Harley
+"""
+import pygame
+
+
+class Cannon(pygame.sprite.Sprite):
+    "Common base class for all cannons"
+
+    cannon_count = 0
+
+    def __init__(self, color):
+        super().__init__()
+        self.image = pygame.Surface([20, 15])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
