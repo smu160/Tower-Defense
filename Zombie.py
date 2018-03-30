d6dbbcf022dd2017b597c225047e6d6f7156495e
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-03-30 00:57:22 -0400

    Utilized rect attribute for collision detection

--- Zombie.py
+++ Zombie.py
@@ -2,24 +2,23 @@
 """
 Created on Sun Mar 11 23:57:17 2018
 
-@author:
+@authors: Saveliy Yusufov and Michael Harley
 """
 
 import pygame
 from spritesheet_functions import SpriteSheet
 
-WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 
 class Zombie(pygame.sprite.Sprite):
     "Common base class for all zombies"
     zombie_count = 0
 
-    def __init__(self, x, y, game_display):
+    def __init__(self, x, y):
         super().__init__()
         self.x = x
         self.y = y
-        self.game_display = game_display
         self.health = 100
         self.list_index = 0
         self.even_or_odd = "even"
@@ -34,17 +33,19 @@ class Zombie(pygame.sprite.Sprite):
         self.walking_frames_list.append(spritesheet.get_image(319, 65, 52, 63))
         self.walking_frames_list.append(spritesheet.get_image(384, 65, 52, 63))
         self.image = self.walking_frames_list[self.list_index]
+        self.rect = self.image.get_rect()
 
-    def move(self, DELTA=0.9):
+    def update(self, DELTA=0.9):
+        """Move a zombie"""
         self.x += DELTA
         if self.even_or_odd == "even":
             self.list_index += 1
             self.image = self.walking_frames_list[self.list_index]
+            self.rect.x = self.x
+            self.rect.y = self.y
             self.even_or_odd = "odd"
         else:
             self.even_or_odd = "even"
-
-        self.game_display.blit(self.image, (self.x, self.y))
 
         if self.list_index == len(self.walking_frames_list)-1:
             self.list_index = 0
