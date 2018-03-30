0c01ab9d1a2eb22434be4140d3df18364badb59f
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-03-29 22:06:46 -0400

    Added functionality to send waves of increasing size and for zombies that get past defenses to lower player health

--- Game.py
+++ Game.py
@@ -5,42 +5,29 @@ Created on Sun Mar 11 20:37:14 2018
 
 @author:
 """
-import pygame
 import random
+import time
+import pygame
 from Zombie import Zombie
 
-WINDOW_WIDTH = 1024
-WINDOW_HEIGHT = 768
-
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
-zombies_past_perimeter = 0
 
 pygame.init()
-game_display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+GAME_DISPLAY = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Tower Defense")
-clock = pygame.time.Clock()
+CLOCK = pygame.time.Clock()
 
-background_img = pygame.image.load("grass_background.png")
-
-tower_img = pygame.image.load("guard_tower.png").convert()
-tower_img.set_colorkey((255, 255, 255))
+zombies_past_perimeter = 0
+background_img = pygame.image.load("grass_background.png").convert()
 
 
-def tower(x, y):
-    game_display.blit(tower_img, (x, y))
-
-
-x_tower = WINDOW_WIDTH * 0.85
-y_tower = WINDOW_HEIGHT * 0.3
-
-
-def make_zombie_herd(num_of_zombies, game_display, WINDOW_HEIGHT):
+def make_zombie_herd(num_of_zombies, game_display):
     """Creates a herd of zombies
 
     Creates a list of zombies generated in random positions on the screen
-    start_x_pos for each zombie is a number in [-100, 0)
+    start_x_pos for each zombie is a number in [-1000, 0)
     start_y_pos for each zombie is a random position along the y-axis
 
     Args:
@@ -52,44 +39,70 @@ def make_zombie_herd(num_of_zombies, game_display, WINDOW_HEIGHT):
     """
     zombie_herd = list()
     for _ in range(0, num_of_zombies):
-        start_x_pos = random.randint(-100, 0)
+        start_x_pos = random.randint(-1000, 0)
         start_y_pos = WINDOW_HEIGHT * random.uniform(0.1, 0.9)
         zombie_herd.append(Zombie(start_x_pos, start_y_pos, game_display))
 
     return zombie_herd
 
 
-def move_zombie_herd(zombie_herd):
+def move_zombie_herd(zombie_group, zombies_past_perim):
     """Moves a herd of zombies
 
     Simply calls move() (with default delta) on
     all the zombies in a given zombie herd
 
     Args:
-        zombie_herd: the herd of zombies to be moved along the display
+        zombie_group: the herd of zombies to be moved along the display
     """
-    for zombie in zombie_herd:
+    for zombie in zombie_group:
+
+        # If a zombie makes it past the perimeter, then
+        if zombie.x > WINDOW_WIDTH:
+            zombies_past_perim += 1
+            zombie_group.remove(zombie)
         zombie.move()
 
 
-zombie_herd = make_zombie_herd(500, game_display, WINDOW_HEIGHT)
+running = True
+gates_of_hell_open = True
+size_of_herd = 10
+herd_of_zombies = make_zombie_herd(size_of_herd, GAME_DISPLAY)
 
-crashed = False
+# Time to begin waiting for next wave of zombies
+begin_wait_time = 0
 
-while not crashed:
+while running and zombies_past_perimeter < 100:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            crashed = True
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_position = pygame.mouse.get_pos()
+            print(mouse_position)
 
-    game_display.blit(background_img, (0, 0))
-    # tower(x_tower, y_tower)
+    GAME_DISPLAY.blit(background_img, (0, 0))
 
-    move_zombie_herd(zombie_herd)
+    # Check if the wave of zombies is empty
+    if not herd_of_zombies:
+        begin_wait_time = time.time()
+        gates_of_hell_open = False
+        size_of_herd += random.randint(3, 6)
+        herd_of_zombies = make_zombie_herd(size_of_herd, GAME_DISPLAY)
+
+    if not gates_of_hell_open:
+        if time.time() - begin_wait_time >= 10:
+            gates_of_hell_open = True
+
+    # If zombie waves are allowed to attack then move them across the screen
+    if gates_of_hell_open:
+        move_zombie_herd(herd_of_zombies, zombies_past_perimeter)
 
     pygame.display.update()
 
     # Frames Per Second (FPS)
-    clock.tick(60)
+    # Will block execution until 1/60 seconds have passed
+    # since the previous time clock.tick was called.
+    CLOCK.tick(60)
 
-pygame.quit()
+pygame.display.quit()
 quit()
