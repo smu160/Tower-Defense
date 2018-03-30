51976440aa65a6066f7c554807f35acdb8637e93
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-03-30 00:57:59 -0400

    Implemented collision detection betwen zombies and bullets

--- Game.py
+++ Game.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sun Mar 11 20:37:14 2018
 
-@author:
+@authors: Saveliy Yusufov and Michael Harley
 """
 import random
 import time
 import pygame
+from bullet import Bullet
+from cannon import Cannon
 from Zombie import Zombie
 
 WINDOW_WIDTH = 800
@@ -23,7 +24,7 @@ zombies_past_perimeter = 0
 background_img = pygame.image.load("grass_background.png").convert()
 
 
-def make_zombie_herd(num_of_zombies, game_display):
+def make_zombie_herd(num_of_zombies):
     """Creates a herd of zombies
 
     Creates a list of zombies generated in random positions on the screen
@@ -35,13 +36,13 @@ def make_zombie_herd(num_of_zombies, game_display):
         game_display: the pygame display intialized in the beginning
         WINDOW_HEIGHT: the height of the display
 
-    Returns: a list of zombies
+    Returns: a sprite group of zombies
     """
-    zombie_herd = list()
-    for _ in range(0, num_of_zombies):
+    zombie_herd = pygame.sprite.Group()
+    for _ in range(num_of_zombies):
         start_x_pos = random.randint(-1000, 0)
         start_y_pos = WINDOW_HEIGHT * random.uniform(0.1, 0.9)
-        zombie_herd.append(Zombie(start_x_pos, start_y_pos, game_display))
+        zombie_herd.add(Zombie(start_x_pos, start_y_pos))
 
     return zombie_herd
 
@@ -49,47 +50,27 @@ def make_zombie_herd(num_of_zombies, game_display):
 def move_zombie_herd(zombie_group, zombies_past_perim):
     """Moves a herd of zombies
 
-    Simply calls move() (with default delta) on
-    all the zombies in a given zombie herd
+    This function calls the zombie class move() function (with default delta)
+    on a given zombie herd. If a zombie makes it past the perimeter, then it
+    increments the zombies_past_perim counter, and removes the zombie from the
+    zombie_group list
 
     Args:
         zombie_group: the herd of zombies to be moved along the display
     """
     for zombie in zombie_group:
-
-        # If a zombie makes it past the perimeter, then
         if zombie.x > WINDOW_WIDTH:
             zombies_past_perim += 1
             zombie_group.remove(zombie)
-        zombie.move()
 
 
 running = True
 gates_of_hell_open = True
 size_of_herd = 10
-herd_of_zombies = make_zombie_herd(size_of_herd, GAME_DISPLAY)
+herd_of_zombies = make_zombie_herd(size_of_herd)
 
 # Time to begin waiting for next wave of zombies
 begin_wait_time = 0
-
-class Cannon(pygame.sprite.Sprite):
-    def __init__(self, color):
-        super().__init__()
-        self.image = pygame.Surface([20, 15])
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-
-class Bullet(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-
-        self.image = pygame.Surface([4, 10])
-        self.image.fill(BLACK)
-
-        self.rect = self.image.get_rect()
-
-    def update(self):
-        self.rect.x -= 3
 
 cannons = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
@@ -108,20 +89,19 @@ while running and zombies_past_perimeter < 100:
             cannon.rect.y = y_mouse
             cannons.add(cannon)
 
-
     bullet = Bullet()
     bullet.rect.x = x_mouse - 25
     bullet.rect.y = y_mouse
     bullets.add(bullet)
-    
 
     cannons.update()
     bullets.update()
+    herd_of_zombies.update()
 
     GAME_DISPLAY.blit(background_img, (0, 0))
 
     # Check if the wave of zombies is empty
-    if not herd_of_zombies:
+    if len(herd_of_zombies) == 0:
         begin_wait_time = time.time()
         gates_of_hell_open = False
         size_of_herd += random.randint(3, 6)
@@ -137,11 +117,13 @@ while running and zombies_past_perimeter < 100:
 
     cannons.draw(GAME_DISPLAY)
     bullets.draw(GAME_DISPLAY)
+    herd_of_zombies.draw(GAME_DISPLAY)
 
     for bullet in bullets:
-        hit_list = pygame.sprite.spritecollide(bullet, cannons, True)
-        for c in hit_list:
+        hit_list = pygame.sprite.spritecollide(bullet, herd_of_zombies, True)
+        for hit_zombie in hit_list:
             bullets.remove(bullet)
+            herd_of_zombies.remove(hit_zombie)
 
         if bullet.rect.x < -10:
             bullets.remove(bullet)
