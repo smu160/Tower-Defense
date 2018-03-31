fcc1a94f99d25d8dd1ebd1935c70a698ebda1c13
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-03-31 01:51:59 -0400

    Implemented draw() function

--- cannon.py
+++ cannon.py
@@ -19,3 +19,7 @@ class Cannon(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+    def draw(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
