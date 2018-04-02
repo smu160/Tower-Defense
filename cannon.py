a9a19b6a224df1d1e6a0f6995a6b63a1b8b5ed42
mharley0111 <mh3686@columbia.edu>
2018-04-01 21:40:20 -0400

    made cannon a moving image in cannon.py

--- cannon.py
+++ cannon.py
@@ -15,14 +15,46 @@ class Cannon(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
         super().__init__()
-        sprite_sheet = SpriteSheet("test_tower.png")
-        self.image = sprite_sheet.get_image(1, 1, 100, 100, "cannon")
+        spritesheet = SpriteSheet("towers.png")
+        self.cannon_frames_list = list()
+        self.cannon_frames_list.append(spritesheet.get_image(116, 0, 100, 107, "cannon")) # tower 1
+        self.cannon_frames_list.append(spritesheet.get_image(227, 0, 100, 107, "cannon")) # tower 2
+        self.cannon_frames_list.append(spritesheet.get_image(338, 0, 100, 107, "cannon")) # tower 3
+        self.cannon_frames_list.append(spritesheet.get_image(449, 0, 100, 107, "cannon")) # tower 4
+        self.cannon_frames_list.append(spritesheet.get_image(560, 0, 100, 107, "cannon")) # tower 5
+        self.cannon_frames_list.append(spritesheet.get_image(671, 0, 100, 107, "cannon")) # tower 6
+        self.cannon_frames_list.append(spritesheet.get_image(782, 0, 100, 107, "cannon")) # tower 7
+        self.cannon_frames_list.append(spritesheet.get_image(893, 0, 100, 107, "cannon")) # tower 8
+        self.cannon_frames_list.append(spritesheet.get_image(1004, 0, 100, 107, "cannon")) # tower 9
+        self.cannon_frames_list.append(spritesheet.get_image(1115, 0, 100, 107, "cannon")) # tower 10
+        self.cannon_frames_list.append(spritesheet.get_image(1226, 0, 100, 107, "cannon")) # tower 11
+        self.cannon_frames_list.append(spritesheet.get_image(1337, 0, 100, 107, "cannon")) # tower 12
+        self.cannon_frames_list.append(spritesheet.get_image(1448, 0, 100, 107, "cannon")) # tower 13
+        self.cannon_frames_list.append(spritesheet.get_image(1559, 0, 100, 107, "cannon")) # tower 14
+        self.cannon_frames_list.append(spritesheet.get_image(1670, 0, 100, 107, "cannon")) # tower 15
+        self.cannon_frames_list.append(spritesheet.get_image(1781, 0, 100, 107, "cannon")) # tower 16
+        self.list_index = 0
+        self.image = self.cannon_frames_list[self.list_index]
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.radius = 75
+        self.x = x
+        self.y = y
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.radius = 175
         self.bullets = pygame.sprite.Group()
         self.firing_rate = 0
+        self.m = 0
+        self.cannons = 5
+
+    def update(self):
+        """Move a zombie"""
+        if self.m % 30 == 0:
+            self.list_index += 1
+            self.image = self.cannon_frames_list[self.list_index] 
+        self.m += 1   
+
+        if self.list_index == len(self.cannon_frames_list)-1:
+            self.list_index = 0
 
     def draw(self, x, y):
         """Updates the bullet position
