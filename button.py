bd32414cc261171077dee6af652ac3b4dca233d9
mharley0111 <mh3686@columbia.edu>
2018-04-01 21:39:48 -0400

    made button image smaller in button.py

--- button.py
+++ button.py
@@ -4,8 +4,9 @@ from spritesheet_functions import SpriteSheet
 class Button(pygame.sprite.Sprite):
 	def __init__(self, x, y, wd, ht):
 		super().__init__()
-		sprite_sheet = SpriteSheet("test_tower.png")
-		self.image = sprite_sheet.get_image(1, 1, 100, 100, "cannon")
+		sprite_sheet = SpriteSheet("towers.png")
+		self.image = sprite_sheet.get_image(116, 0, 100, 107, "cannon")
+		self.image = pygame.transform.scale(self.image, (wd, ht))
 		self.rect = self.image.get_rect()
 		self.x = x
 		self.y = y
@@ -13,40 +14,19 @@ class Button(pygame.sprite.Sprite):
 		self.ht = ht
 		self.rect.x = x
 		self.rect.y = y
-		self.rect.width = wd
-		self.rect.height = ht
-
-
-		#self.game_display = game_display
-		#self.color = color
-		#self.wd = wd
-		#self.ht = ht
-		#self.x = x
-		#self.y = y
-		#self.image = pygame.Surface([self.wd, self.ht])
-		#self.image.fill(color)
-		#self.rect = self.image.get_rect()
-		#self.rect.x = x
-		#self.rect.y = y
-		#pygame.draw.rect(self.game_display, self.color, pygame.Rect(self.x, self.y, self.wd, self.ht))
+		self.rect.width = self.wd 
+		self.rect.height = self.ht 
 
 	def update(self, shrink):
 		if shrink == "shrink":
-			#self.image = pygame.Surface([self.wd - 10, self.ht - 10])
 			self.rect.width = self.wd - 10
 			self.rect.height = self.ht - 10
 			self.rect.x = self.x + 5
 			self.rect.y = self.y + 5
-			#pygame.draw.rect(self.game_display, self.color, pygame.Rect(self.x + 5, self.y + 5, self.wd - 10, self.ht - 10))
 		else:
-			#self.image = pygame.Surface([self.wd, self.ht])
 			self.rect.width = self.wd
 			self.rect.height = self.ht
 			self.rect.x = self.x
 			self.rect.y = self.y
-			#pygame.draw.rect(self.game_display, self.color, pygame.Rect(self.x, self.y, self.wd, self.ht))
-		#self.image.fill(self.color)
-
-		# display button image
 
 		
